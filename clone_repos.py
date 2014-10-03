9769970d3be7fd9422a97bdc446fd108cc1b3c31
Ubuntu <ubuntu@ip-172-31-44-70.us-west-2.compute.internal>
2014-10-03 20:47:46 +0000

    uploaded changes to clone_repos.py

--- clone_repos.py
+++ clone_repos.py
@@ -1,20 +1,25 @@
 import subprocess
+import time
 
 # if storage is almost full or force is set to true, rsync
 def check_and_sync_storage(force):
 	threshold = 90 # corresponds to over 720 GB used on i2.xlarge
-	results = subprocess.check_output('df -kh .', shell=True)
-	used = results.split('\n')[1].split()[4].strip().rstrip('%') # grab fifth element of 2nd line and toss all but the pct
+	try: results = subprocess.check_output('df -kh .', shell=True)
+	except Exception: return
+	used = int(results.split('\n')[1].split()[4].strip().rstrip('%')) # grab fifth element of 2nd line and toss all but the pct
 	if used > threshold or force:
-		pass
-		'''
-		rsync it to da2
-		'''
+		try:
+			subprocess.call('rsync -ae \'ssh -p2200\' ~/repos jduggan1@da2.eecs.utk.edu:', shell=True)
+			subprocess.call('rm -r ~/repos', shell=True)
+			subprocess.call('mkdir ~/repos', shell=True)
+		except Exception:
+			return
 
 # call the given command and return the time it took to finish
 def call_and_time(cmd):
 	start = time.time()
-	subprocess.call(cmd, shell=True)
+	try: subprocess.call(cmd, shell=True)
+	except Exception: pass
 	return time.time() - start
 
 # just your average declarations
@@ -33,17 +38,17 @@ if len(sizes) != len(divided):
 
 # loop through the repo list, cloning each of our repos
 for i in range(len(sizes)):
+	print i
 	# grab the size, group, version control system, and repo name info from the files
 	size = int(sizes[i].split(';')[0].strip())
 	group,vcs,repo = map(str.strip, divided[i].split(';'))
 	group = int(group)
 	team,name = repo.split('/')
 
-	# check the current amount of storage used, rsync if too much
-	check_and_sync_storage(False)
-
-	# if the repo is our responsibility, let's grab it
 	if group == our_group:
+		# check the current amount of storage used, rsync if too much
+		check_and_sync_storage(False)
+
 		if vcs == 'hg':
 			cmd = 'hg clone -U https://bitbucket.org/{0}/{1} ~/repos/{0}_{1}'.format(team, name)
 			elapsed = call_and_time(cmd)
