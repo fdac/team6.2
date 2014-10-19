f28c6896f66a2a6075b9eb0fb16ab2aa8b37331d
jduggan1 <jduggan1@github.com>
2014-10-19 17:00:21 -0400

    added code to crawl through our list of repos and print the commit url

--- populate_commits.py
+++ populate_commits.py
@@ -1,4 +1,19 @@
 import re
 import pymongo, json
 
-client = pymongo.MongoClient('localhost')
+client = pymongo.MongoClient(host='da0.eecs.utk.edu')
+db = client['bitbucket']
+lines = open('divided', 'r').readlines()
+our_group = 6
+
+repos = db['repos']
+for line in lines:
+	tokens = map(str.strip, line.strip().split(';'))
+	group = int(tokens[0])
+	scm = tokens[1]
+	name = tokens[2]
+
+	if group == our_group:
+		key = 'https://bitbucket.org/2.0/repositories/' + name
+		key = key.strip('\.')
+		url = repos[key]['links']['commits']['href']
